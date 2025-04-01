84767d468ca9cfb2103678d3651264192e0fa92a
irvingmagana <imagana@walook.com.mx>
2025-04-01 17:43:35 -0600

    .

--- src/services/genericos/date.service.js
+++ src/services/genericos/date.service.js
@@ -1,13 +1,6 @@
-const obtenerFechaHora = () => {
-  const ahora = new Date()
+import moment from 'moment-timezone'
+import { TZ } from '../../commons/constants.js'
 
-  const dd = String(ahora.getDate()).padStart(2, '0')
-  const mm = String(ahora.getMonth() + 1).padStart(2, '0')
-  const aa = String(ahora.getFullYear()).slice(-2)
-  const hh = String(ahora.getHours()).padStart(2, '0')
-  const min = String(ahora.getMinutes()).padStart(2, '0')
-
-  return `${dd}${mm}${aa}${hh}${min}`
-}
+const obtenerFechaHora = () => moment().tz(TZ).format('DDMMYYHHmm')
 
 export { obtenerFechaHora }
--- src/services/genericos/file.service.js
+++ src/services/genericos/file.service.js
@@ -1,26 +1,25 @@
 import archiver from 'archiver'
 import fs from 'fs'
 import LOG from '../../commons/logger.js'
+import zipEncrypted from 'archiver-zip-encrypted' // Importación estática
+
+// Registrar el formato de cifrado una sola vez
+archiver.registerFormat('zip-encrypted', zipEncrypted)
 
 const crearZipConPass = async (outputPath, file, password) => {
   try {
-    const { default: zipEncrypted } = await import('archiver-zip-encrypted')
-    
-    // Registrar el formato de cifrado
-    archiver.registerFormat('zip-encrypted', zipEncrypted)
-
     const output = fs.createWriteStream(outputPath)
     const archive = archiver('zip-encrypted', {
       zlib: { level: 9 },
       encryptionMethod: 'aes256',
-      password: password
+      password
     })
 
     output.on('close', () => {
       LOG.info(`Archivo ZIP creado con contraseña: ${outputPath}`)
     })
 
-    archive.on('error', (err) => {
+    archive.on('error', err => {
       throw err
     })
 
@@ -28,9 +27,9 @@ const crearZipConPass = async (outputPath, file, password) => {
     archive.file(file, { name: file.split('/').pop() })
     await archive.finalize()
   } catch (error) {
-     LOG.error('Error al generar el archivo ZIP')
-     LOG.error(error)
+    LOG.error('Error al generar el archivo ZIP')
+    LOG.error(error)
   }
-};
+}
 
 export { crearZipConPass }
